49ecf5b4e618d57a1f8097628998a7fb50150d77
Roy <roy.avishekroy60@outlook.com>
2024-05-22 06:57:24 +0000

    Revert "mod2v2"
    This reverts commit 43accb48236498b05321387e6aea1de0a0744033.

--- module2.py
+++
@@ -1,9 +0,0 @@
-# import the time module
-import time
-
-# get the current time in seconds since the epoch
-seconds = time.time()
-
-print("Seconds since epoch =", seconds)	
-
-# Output: Seconds since epoch = 1672214933.6804628
